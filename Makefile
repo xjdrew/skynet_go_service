0ce2e5c9e88222a1aed4518b93a888be6825c0fc
xjdrew <xj.drew@gmail.com>
2019-09-17 21:32:43 +0800

    support callback

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	CGO_CFLAGS='-I /home/tiger/work/tadpole/template/build/include' go build -buildmode=c-shared -o gos.so gos.go
+	CGO_CFLAGS='-I /home/tiger/work/tadpole/template/build/include' CGO_LDFLAGS='-Wl,--unresolved-symbols=ignore-all' go build -buildmode=c-shared -o gos.so gos.go
 clean:
 	-rm gos.go
--- gos.go
+++ gos.go
@@ -1,12 +1,24 @@
 package main
 
-// #include "skynet.h"
+/*
+#include "skynet.h"
+extern int gos_cb(struct skynet_context* context, void* ptr, int typ, int session, int source, char* msg, int sz);
+
+static int gos_cb_wrapper(struct skynet_context * context, void *ud, int type, int session, uint32_t source , const void * msg, size_t sz) {
+	gos_cb(context, ud, type, session, (int)source, (char*)msg, (int)sz);
+}
+
+static inline void set_gos_callback(struct skynet_context * context, uintptr_t ud) {
+	skynet_callback(context, (void*)ud, gos_cb_wrapper);
+}
+*/
 import "C"
 
 import (
 	"log"
 	"sync"
 	"sync/atomic"
+	"unsafe"
 )
 
 type gosEnv struct {
@@ -27,6 +39,13 @@ func getEnv(ptr uintptr) *gosEnv {
 	return env.(*gosEnv)
 }
 
+//export gos_cb
+func gos_cb(context *C.struct_skynet_context, ptr unsafe.Pointer, typ C.int, session C.int, source C.int, s *C.char, sz C.int) C.int {
+	msg := C.GoStringN(s, sz)
+	log.Printf("gos cb: from %d, session: %d, msg:%s", source, session, msg)
+	return 0
+}
+
 //export gos_create
 func gos_create() uintptr {
 	handle := atomic.AddUint32(&nextHandle, 1)
@@ -44,6 +63,7 @@ func gos_init(ptr uintptr, ctx *C.struct_skynet_context, args *C.char) {
 	env := getEnv(ptr)
 	env.args = C.GoString(args)
 	log.Print("gos init:", env.handle, env.args)
+	C.set_gos_callback(ctx, C.uintptr_t(ptr))
 	return
 }
 
